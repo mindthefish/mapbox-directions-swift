31bd8ca8244d26c25deb1931934231d8205c8be6
Konrad Tuschy <konrad.tuschy@bareways.com>
2023-06-29 10:33:53 +0200

    feat: add first version of Package.swift

---
+++ Package.swift
@@ -0,0 +1,50 @@
+// swift-tools-version:5.8
+
+import PackageDescription
+
+let package = Package(
+    name: "MapboxDirections",
+    platforms: [
+        .macOS(.v10_14), .iOS(.v12), .watchOS(.v5), .tvOS(.v12)
+    ],
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "MapboxDirections",
+            targets: ["MapboxDirections"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        .package(name: "OHHTTPStubs", url: "https://github.com/AliSoftware/OHHTTPStubs.git", from: "6.0.0"),
+		.package(name: "Polyline", url: "https://github.com/raphaelmor/Polyline.git", revision: "v5.1.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "MapboxDirections",
+            exclude: [
+                "Info.plist",
+                "MBAttribute.h",
+				"MBLaneIndication.h",
+				"MBRoadClasses.h",
+				"MBRouteOptions.h",
+				"MBRouteOptions.m",
+                "MBPlacemarkPrecision.m",
+                "MBPlacemarkScope.h",
+            ]),
+        .testTarget(
+            name: "MapboxDirectionsTests",
+            dependencies: [
+                "MapboxDirections",
+                .product(name: "OHHTTPStubsSwift", package: "OHHTTPStubs"),
+				.product(name: "Polyline", package: "Polyline"),
+            ],
+            exclude: [
+                "Info.plist",
+            ],
+            resources: [
+                .process("Fixtures"),
+            ]),
+    ]
+)
