b7c12f8ec24a092cbe89b5e16e338802fb2a4ecf
Konrad Tuschy <konrad.tuschy@bareways.com>
2023-06-29 11:25:21 +0200

    fix: autoformat Package.swift

--- Package.swift
+++ Package.swift
@@ -5,18 +5,19 @@ import PackageDescription
 let package = Package(
     name: "MapboxDirections",
     platforms: [
-        .macOS(.v10_14), .iOS(.v12), .watchOS(.v5), .tvOS(.v12)
+        .macOS(.v10_14), .iOS(.v12), .watchOS(.v5), .tvOS(.v12),
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "MapboxDirections",
-            targets: ["MapboxDirections"]),
+            targets: ["MapboxDirections"]
+        ),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/AliSoftware/OHHTTPStubs.git", from: "6.0.0"),
-		.package(url: "https://github.com/raphaelmor/Polyline.git", revision: "v5.1.0"),
+        .package(url: "https://github.com/raphaelmor/Polyline.git", revision: "v5.1.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -24,30 +25,32 @@ let package = Package(
         .target(
             name: "MapboxDirections",
             dependencies: [
-                .product(name: "Polyline", package: "Polyline")
+                .product(name: "Polyline", package: "Polyline"),
             ]
             exclude: [
                 "Info.plist",
                 "MBAttribute.h",
-				"MBLaneIndication.h",
-				"MBRoadClasses.h",
-				"MBRouteOptions.h",
-				"MBRouteOptions.m",
+                "MBLaneIndication.h",
+                "MBRoadClasses.h",
+                "MBRouteOptions.h",
+                "MBRouteOptions.m",
                 "MBPlacemarkPrecision.m",
                 "MBPlacemarkScope.h",
-            ]),
+            ]
+        ),
         .testTarget(
             name: "MapboxDirectionsTests",
             dependencies: [
                 "MapboxDirections",
                 .product(name: "OHHTTPStubsSwift", package: "OHHTTPStubs"),
-				.product(name: "Polyline", package: "Polyline"),
+                .product(name: "Polyline", package: "Polyline"),
             ],
             exclude: [
                 "Info.plist",
             ],
             resources: [
                 .process("Fixtures"),
-            ]),
+            ]
+        ),
     ]
 )
